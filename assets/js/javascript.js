264b439e7ac7a6afdc2aea2e6d9ad7b810ac53ea
TheBigChungus3 <u3222768@uni.canberra.edu.au>
2025-04-14 15:28:00 +1000

    complete hamburger menu
    hamburger menu size
    javascript for nav-head padding
    nav icons-hover
    update index
    remove home page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,20 +1,55 @@
 // section names
 let header = document.querySelector("header");
 let burger = document.querySelector(".hamburger-menu");
+let navHead = document.querySelector("#nav-head");
+
+var maxW = window.matchMedia("(max-width: 820px)");
 
 function centerBurger() {
+
     var headerHeight = header.offsetHeight;
-    var burgerHeight = burger.offsetHeight;    
+    var burgerHeight = burger.offsetHeight;
 
     var burgerY = (headerHeight/2) - (burgerHeight/2);
     burger.style.top = burgerY + "px";
+
+    console.log("1 header height: ", header.offsetHeight);
+    console.log("1 burger height: ", burger.offsetHeight);
+    console.log("1 burger y: ", burgerY);
+    console.log("1 Burger y (viewport): ", burger.getBoundingClientRect().top, "px");
 }
+
+function phoneNavPad(maxW) {
+
+    var headerHeight = header.offsetHeight;
+    // var burgerHeight = burger.offsetHeight;
+    var navPadTop = headerHeight;
+    var navPadBottom = navPadTop / 1.5;
+
+    if (maxW.matches) {
+        // padding for the phone nav under burger menu
+        navHead.style.paddingTop = navPadTop + "px";
+        navHead.style.paddingBottom = navPadBottom + "px";
+    }
+    else {
+        // padding
+        navHead.style.paddingTop = "";
+        navHead.style.paddingBottom = "";
+    }
+    console.log("1 head navPad: ", navPadTop);
+}
+
+phoneNavPad(maxW);
 
 window.addEventListener("load", centerBurger);
 window.addEventListener("resize", centerBurger);
+maxW.addEventListener("change", function() {
+    phoneNavPad(maxW);
+});
 
 
 // debugging
-console.log("header height: ", header.offsetHeight);
-console.log("burger height: ", burger.offsetHeight);
-console.log("Burger Y (viewport): ", burger.getBoundingClientRect().top, "px");
+console.log("2 header height: ", header.offsetHeight);
+console.log("2 burger height: ", burger.offsetHeight);
+// console.log("2 burger y: ", burgerY);
+console.log("2 Burger y (viewport): ", burger.getBoundingClientRect().top, "px");
